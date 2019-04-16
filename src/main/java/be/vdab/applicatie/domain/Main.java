38184110d5362bff8201f401d00dc8c349f98be0
withouckvincent <withouck.vincent@gmail.com>
2019-04-16 08:33:54 +0200

    Eerste Commit

---
+++ src/main/java/be/vdab/applicatie/domain/Main.java
@@ -0,0 +1,10 @@
+package be.vdab.applicatie.domain;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallo");
+	}
+
+}
